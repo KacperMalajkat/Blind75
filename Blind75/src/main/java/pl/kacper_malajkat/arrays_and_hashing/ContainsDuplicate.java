254acf47a75925c24ffcc0ba778ac2ab623e0349
kacper <kacper.malajkat@gmail.com>
2024-01-24 22:00:49 +0100

    add T and S complexity

--- Blind75/src/main/java/pl/kacper_malajkat/arrays_and_hashing/ContainsDuplicate.java
+++ Blind75/src/main/java/pl/kacper_malajkat/arrays_and_hashing/ContainsDuplicate.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
     Given an integer array nums,
     return true if any value appears at least twice in the array,
     and return false if every element is distinct.
+
+    T: O(N)
+    S: O(N)
 */
 class ContainsDuplicate {
 
